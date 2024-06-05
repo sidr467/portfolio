0fef635d4cdfbb118ef267068b103ec3f1aa4e3f
sidr467 <146876430+sidr467@users.noreply.github.com>
2024-06-05 17:45:12 +0530

    Mobile navbar style finished

--- src/app/components/Menu.tsx
+++ src/app/components/Menu.tsx
@@ -17,11 +17,30 @@ function Menu() {
         onClick={() => setOpen((prev) => !prev)}
       />
       {open && (
-        <div className=" absolute bg-gray-900 left-0 top-20 h-[calc(100vh-80x)]">
-          <Link href="/">About</Link>
-          <Link href="/">Skills</Link>
-          <Link href="/">Experience</Link>
-          <Link href="/">Projects</Link>{" "}
+        <div className=" absolute bg-gray-900 left-0 top-20 w-full h-[calc(100vh-80px)] flex flex-col items-center justify-start gap-8 z-10 text-lg pt-10">
+          <Link href="/" className=" hover:border-b-2 border-white">
+            About
+          </Link>
+          <Link href="/" className=" hover:border-b-2 border-white">
+            Skills
+          </Link>
+          <Link href="/" className=" hover:border-b-2 border-white">
+            Experience
+          </Link>
+          <Link href="/" className=" hover:border-b-2 border-white">
+            Projects
+          </Link>{" "}
+          <button className="hover:ring-2 ring-white rounded-full p-2">
+            <Image
+              src="./nightmode.svg"
+              alt="nightmodeIcon"
+              width={24}
+              height={24}
+            ></Image>
+          </button>
+          <button className="bg-white text-black px-4 py-2 rounded-lg font-semibold">
+            Download CV
+          </button>
         </div>
       )}
     </div>
--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -7,7 +7,7 @@ function NavBar() {
     <nav className=" h-20 ring-1 ring-white">
       {/* MObile */}
       <div className=" h-full md:hidden flex justify-between items-center relative px-4 py-2">
-        <h1>
+        <h1 className=" text-xl font-bold tracking-wider">
           <Link href="/">&lt;Sid/&gt;</Link>
         </h1>
         <Menu />
