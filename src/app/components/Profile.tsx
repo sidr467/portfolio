51d5e340fb16bee64498ea9cf40a16eacf3f23ae
sidr467 <146876430+sidr467@users.noreply.github.com>
2024-06-07 17:31:53 +0530

    added social icons and links

--- src/app/components/Profile.tsx
+++ src/app/components/Profile.tsx
@@ -2,19 +2,19 @@ import Image from "next/image"
 
 function Profile() {
   return (
-    <div className="grid items-center justify-center gap-8">
+    <div className="flex items-center justify-center flex-col gap-8 md:grid grid-cols-2">
       {/* Image Section */}
-      <div className=" bg-white pt-2 rounded-lg  ">
+      <div className=" md:order-2 ">
         <Image
           src="/profileImg.png"
           alt="Profile Image"
           width={150}
           height={200}
-          className=" object-cove rounded-lg"
+          className=" object-cover rounded-lg m-auto self-center"
         />
       </div>
       {/* Text section */}
-      <div className="grid gap-4">
+      <div className="grid gap-6">
         <h1 className=" text-xl font-bold">
           Hi, I&apos;m <strong>Siddharth Rathod</strong>
         </h1>
@@ -27,10 +27,31 @@ function Profile() {
           experience in enterprise companies and startups. Proficient in Html,
           Tailwind Css, Javascript and React. Passionate about UI/UX
         </p>
-        <div className="flex gap-4">
-            <Image src="./github.svg" alt="github icon" width={24} height={24} ></Image>
-            <Image src="./github.svg" alt="github icon" width={24} height={24} ></Image>
-            <Image src="./github.svg" alt="github icon" width={24} height={24} ></Image>
+        <div className="flex gap-4 items-center ">
+          <a href="https://github.com/sidr467">
+            <Image
+              src="./github.svg"
+              alt="github icon"
+              width={24}
+              height={24}
+            ></Image>
+          </a>
+          <a href="mailto:rsiddharth467@gmail.com">
+            <Image
+              src="./gmail.svg"
+              alt="gmail icon"
+              width={24}
+              height={24}
+            ></Image>
+          </a>
+          <a href="https://www.linkedin.com/in/siddharth-rathod-6524b926b/">
+            <Image
+              src="./linkedin.svg"
+              alt="linkedin icon"
+              width={24}
+              height={24}
+            ></Image>
+          </a>
         </div>
       </div>
     </div>
