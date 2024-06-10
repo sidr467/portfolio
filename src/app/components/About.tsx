0bdc63a4a5dc5487690984716ae7542ca3c19f61
sidr467 <146876430+sidr467@users.noreply.github.com>
2024-06-10 22:27:43 +0530

    added some styles in about

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -1,7 +1,7 @@
 function About() {
   return (
-    <div className="flex flex-col justify-center items-center gap-4 lg:gap-8  ">
-      <h2 className=" text-base lg:text-lg">ABOUT</h2>
+    <div className="flex flex-col gap-4 items-center lg:gap-8  ">
+      <h2 className="text-center text-base lg:text-lg font-bold py-2 px-4 ring-1 ring-black dark:ring-white w-fit rounded-lg  ">ABOUT</h2>
       <p className="">
         A Full Stack Developer specializing in React.js and Node.js. I focus on
         creating fast, accessible, visually appealing, and responsive digital
