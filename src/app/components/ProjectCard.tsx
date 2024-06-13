1820cd7592fe3da811653c17d65d020ec53b3ff5
sidr467 <146876430+sidr467@users.noreply.github.com>
2024-06-13 18:10:17 +0530

    some styles in project card

--- src/app/components/ProjectCard.tsx
+++ src/app/components/ProjectCard.tsx
@@ -1,20 +1,31 @@
 import React from "react"
 import Image from "next/image"
 
-function ProjectCard({ title, image, github, live }: any) {
+function ProjectCard({ title, image, github, live, gitsvg }: any) {
   return (
-    <div>
+    <div className="grid gap-4 shadow-sm shadow-slate-600 p-4 rounded-lg">
       <div>
-        <Image src={image} alt={title} width={400} height={400} className=" object-contain" ></Image>
+        <Image
+          src={image}
+          alt={title}
+          width={400}
+          height={400}
+          className=" object-contain"
+        ></Image>
       </div>
-      <div>
-        <h2>{title}</h2>
-        <div>
+      <div className="flex flex-col gap-1 justify-center items-center">
+        <h2 className="text-base md:text-lg font-semibold">{title}</h2>
+        <div className="text-sm md:text-base flex gap-2">
           <a href={github} target="blank">
-            <button>Github</button>
+            <button className="dark:bg-white bg-black text-white dark:text-black rounded-md px-2 py-1 font-medium">
+              <Image src={gitsvg} alt="gitsvg" height={20} width={20}></Image>
+              Github
+            </button>
           </a>
           <a href={live} target="blank">
-            <button>Live</button>
+            <button className="dark:bg-white bg-black text-white dark:text-black rounded-md px-2 py-1 font-medium">
+              Live
+            </button>
           </a>
         </div>
       </div>
--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -7,18 +7,21 @@ function Projects() {
       image: "/pf.png",
       github: "https://github.com/sidr467",
       live: "https://github.com/sidr467",
+      gitsvg: "/gitSvg.svg",
     },
     {
       title: "Portfolio",
       image: "/pf.png",
       github: "https://github.com/sidr467",
       live: "https://github.com/sidr467",
+      gitsvg: "/gitSvg.svg",
     },
     {
       title: "Portfolio",
       image: "/pf.png",
       github: "https://github.com/sidr467",
       live: "https://github.com/sidr467",
+      gitsvg: "/gitSvg.svg",
     },
   ]
 
@@ -30,14 +33,16 @@ function Projects() {
       <h2 className="text-center text-base lg:text-lg font-bold py-2 px-4 ring-1 ring-black dark:ring-white w-fit rounded-lg  ">
         Projects
       </h2>
-      <div>
-        {projects.map((project,index:number)=> (
-            <ProjectCard 
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 min-w-[320px] gap-8">
+        {projects.map((project, index: number) => (
+          <ProjectCard
             key={index}
             image={project.image}
             title={project.title}
             github={project.github}
-            live={project.live} />
+            live={project.live}
+            gitSvg={project.gitsvg}
+          />
         ))}
       </div>
     </div>
