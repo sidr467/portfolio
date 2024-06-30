67c29712e1fede876e947161aa5234f69663db58
sidr467 <146876430+sidr467@users.noreply.github.com>
2024-06-30 10:28:36 +0530

    made new file for icons skills

--- src/app/components/Skills.tsx
+++ src/app/components/Skills.tsx
@@ -1,85 +1,4 @@
-import Image from "next/image"
-import { SiJavascript } from "react-icons/si"
-import { SiTypescript } from "react-icons/si"
-import { SiReact } from "react-icons/si"
-import { RiNextjsLine } from "react-icons/ri"
-import { FaNodeJs } from "react-icons/fa6"
-import { SiExpress } from "react-icons/si"
-import { SiTailwindcss } from "react-icons/si"
-import { SiMongodb } from "react-icons/si"
-import { FaGitAlt } from "react-icons/fa6"
-import { FaHtml5 } from "react-icons/fa"
-import { FaCss3Alt } from "react-icons/fa"
-import { BsFillBootstrapFill } from "react-icons/bs"
-// import { skillIcons } from "./Skills"
-
-//Skills react icons
-const skillIcons = [
-  {
-    name: "Javascript",
-    svg: <SiJavascript className="w-12 h-12 fill-yellow-400 rounded-sm" />,
-  },
-  {
-    name: "Typescript",
-    svg: (
-      <SiTypescript className="w-12 h-12 fill-blue-500 bg-white rounded-sm" />
-    ),
-  },
-  {
-    name: "React",
-    svg: <SiReact className="w-12 h-12 fill-blue-500" />,
-  },
-  {
-    name: "NextJS",
-    svg: <RiNextjsLine className="w-12 h-12 fill-current" />,
-  },
-  {
-    name: "NodeJS",
-    svg: <FaNodeJs className="w-12 h-12 fill-[#5FA04E]" />,
-  },
-  {
-    name: "MongoDB",
-    svg: <SiMongodb className="w-12 h-12 fill-green-800" />,
-  },
-  {
-    name: "ExpressJS",
-    svg: (
-      <SiExpress className="w-12 h-12 dark:bg-current fill-black p-2 rounded-sm" />
-    ),
-  },
-  {
-    name: "Tailwind",
-    svg: <SiTailwindcss className="w-12 h-12 fill-[#38BDF8]" />,
-  },
-  {
-    name: "Figma",
-    svg: (
-      <Image
-        src="/figma.svg"
-        alt="figmaSVG"
-        width={50}
-        height={50}
-        className="w-12 h-12 object-contain"
-      ></Image>
-    ),
-  },
-  {
-    name: "Git",
-    svg: <FaGitAlt className="w-12 h-12 fill-[#F4511E]" />,
-  },
-  {
-    name: "HTML",
-    svg: <FaHtml5 className="w-12 h-12 fill-orange-500" />,
-  },
-  {
-    name: "CSS",
-    svg: <FaCss3Alt className="w-12 h-12 fill-blue-500" />,
-  },
-  {
-    name: "Bootstrap",
-    svg: <BsFillBootstrapFill className="w-12 h-12 fill-violet-700" />,
-  },
-]
+import { skillicons } from "./skillicons"
 
 function Skills() {
   return (
@@ -89,7 +8,7 @@ function Skills() {
       </h2>
       <div className="mx-auto container p-4">
         <div className=" grid gap-x-4 gap-y-8 justify-center items-center grid-cols-3 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-6 xl:grid-cols-8">
-          {skillIcons.map((skill: any, index: any) => (
+          {skillicons.map((skill: any, index: any) => (
             <div key={index} className="flex flex-col items-center gap-2">
               {skill.svg}
               <p className="text-xs font-semibold">{skill.name}</p>
